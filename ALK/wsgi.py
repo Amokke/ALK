5e50450e3bc18151c7914eb4d1e45092f615f6cb
Amokke <24.02.1985@mail.ru>
2025-04-03 10:40:53 +0700

    add project

---
+++ ALK/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ALK project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ALK.settings')
+
+application = get_wsgi_application()
